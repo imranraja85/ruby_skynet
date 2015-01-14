f5c391b851321876006b7d91c1160e6a2c1ab141
Imran Raja <imranraja85@gmail.com>
2015-01-14 17:40:51 -0500

    refactoring

--- lib/ruby_skynet/algorithms/simple_linear_regression.rb
+++ lib/ruby_skynet/algorithms/simple_linear_regression.rb
@@ -1,27 +1,28 @@
 module RubySkynet
   module Algorithms
     class SimpleLinearRegression
+      attr_reader :x_coordinates, :y_coordinates
 
-      def initialize(xs, ys)
-        @xs, @ys = xs, ys
-        if @xs.length != @ys.length
-          raise "Unbalanced data. xs need to be same length as ys"
-        end
+      def initialize(coordinates)
+        raise "You should pass in an array of coordinates" if coordinates.empty?
+
+        @x_coordinates = coordinates.map(&:first)
+        @y_coordinates = coordinates.map(&:last)
       end
 
       def y_intercept
-        mean(@ys) - (slope * mean(@xs))
+        mean(y_coordinates) - (slope * mean(x_coordinates))
       end
 
       def slope
-        x_mean = mean(@xs)
-        y_mean = mean(@ys)
+        x_mean = mean(x_coordinates)
+        y_mean = mean(y_coordinates)
 
-        numerator = (0...@xs.length).reduce(0) do |sum, i|
-          sum + ((@xs[i] - x_mean) * (@ys[i] - y_mean))
+        numerator = (0...x_coordinates.length).reduce(0) do |sum, i|
+          sum + ((x_coordinates[i] - x_mean) * (y_coordinates[i] - y_mean))
         end
 
-        denominator = @xs.reduce(0) do |sum, x|
+        denominator = x_coordinates.reduce(0) do |sum, x|
           sum + ((x - x_mean) ** 2)
         end
 
@@ -29,10 +30,9 @@ module RubySkynet
       end
 
       def mean(values)
-        total = values.reduce(0) { |sum, x| x + sum }
-        Float(total) / Float(values.length)
+        total = values.reduce(&:+)
+        total.to_f / values.length
       end
-
     end
   end
 end
--- spec/ruby_skynet/algorithms/simple_linear_regression_spec.rb
+++ spec/ruby_skynet/algorithms/simple_linear_regression_spec.rb
@@ -4,8 +4,8 @@ describe RubySkynet::Algorithms::SimpleLinearRegression do
   subject {described_class}
 
   it "has a y intercept of 1 and slope of 0" do
-    xs, ys = [0,1,2], [1,1,1]
-    calc = subject.new(xs, ys)
+    coordinates = [[0,1], [1,1], [2,1]]
+    calc = subject.new(coordinates)
     expect(calc.y_intercept).to eq(1)
     expect(calc.slope).to eq(0)
   end
